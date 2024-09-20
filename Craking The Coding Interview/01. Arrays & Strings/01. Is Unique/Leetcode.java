1156bbbb8822950bedc9f8ff4f26e11249ff0840
abhisheksdtu <abhisheksdtu@gmail.com>
2024-09-20 09:23:31 +0000

    Is Unique Leetcode

---
+++ Craking The Coding Interview/01. Arrays & Strings/01. Is Unique/Leetcode.java
@@ -0,0 +1,32 @@
+/*
+    Question - 217. Contains Duplicate - https://leetcode.com/problems/contains-duplicate/description/
+*/
+
+// TC - O(N)
+// SC - O(N)
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
+
+// TC - O(N * LOG N)
+// SC - O(1)
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] == nums[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
