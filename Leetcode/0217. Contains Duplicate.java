1f3a0343f92439ef78dc0d32e4e3ea3353e9487a
abhisheksdtu <abhisheksdtu@gmail.com>
2024-09-20 09:39:18 +0000

    leetcode 217

--- Leetcode/0217. Contains Duplicate.java
+++ Leetcode/0217. Contains Duplicate.java
@@ -1,3 +1,5 @@
+// TC - O(N)
+// SC - O(N)
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<>();
@@ -6,6 +8,20 @@ class Solution {
                 return true;
             }
             set.add(num);
+        }
+        return false;
+    }
+}
+
+// TC - O(N * LOG N)
+// SC - O(1)
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] == nums[i]) {
+                return true;
+            }
         }
         return false;
     }
