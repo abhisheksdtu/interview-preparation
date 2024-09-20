52ef35653b57807ed8455f8b6b89383e4db32c02
abhisheksdtu <abhisheksdtu@gmail.com>
2024-09-20 11:58:52 +0000

    leetcode-234

---
+++ Leetcode/0234. Palindrome Linked List.java
@@ -0,0 +1,72 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode mid = middle(head);
+        ListNode newHead = mid.next;
+        mid.next = null;
+        newHead = reverse(newHead);
+
+        ListNode p1 = head;
+        ListNode p2 = newHead;
+
+        while (p2 != null) {
+            if (p1.val != p2.val) {
+                return false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        newHead = reverse(newHead);
+        mid.next = newHead;
+
+        return true;
+    }
+
+    public ListNode middle(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        return prev;
+    }
+}
