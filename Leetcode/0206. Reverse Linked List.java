e761ca0cc506ed65888951f791bf4f9cf2487c63
abhisheksdtu <abhisheksdtu@gmail.com>
2024-09-20 11:44:37 +0000

    leetcode-206, leetcode-876

--- Leetcode/0206. Reverse Linked List.java
+++ Leetcode/0206. Reverse Linked List.java
@@ -10,6 +10,10 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
         ListNode prev = null;
         ListNode curr = head;
         while (curr != null) {
--- Leetcode/0876. Middle of the Linked List.java
+++ Leetcode/0876. Middle of the Linked List.java
@@ -10,6 +10,10 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
         ListNode slow = head;
         ListNode fast = head;
 
