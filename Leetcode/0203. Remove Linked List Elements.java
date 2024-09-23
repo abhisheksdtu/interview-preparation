5d9b4296359e8b3101f3470c3e55a21abeba433c
abhisheksdtu <abhisheksdtu@gmail.com>
2024-09-23 17:50:05 +0000

    leetcode-203

--- Leetcode/0203. Remove Linked List Elements.java
+++ Leetcode/0203. Remove Linked List Elements.java
@@ -8,6 +8,21 @@
  * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+/*
+    APPROACH
+    1. Create a dummy node for handling edge cases where the head node might need to be removed and point it to head.
+    2. Initialize a pointer starting at dummy node to iterate over the list.
+    3. If the next node's value matches 'val', skip the node by adjusting pointers.
+    4. If the value does not match, move the pointer to the next node.
+    5. Return the list starting from 'dummy.next', skipping the dummy node.
+
+    TIME COMPLEXITY
+    - O(N), as each node is visited once.
+
+    SPACE COMPLEXITY
+    - O(1), using only a dummy node and constant space.
+*/
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
         if (head == null) {
