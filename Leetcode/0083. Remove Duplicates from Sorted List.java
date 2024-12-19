0107fc413cdaea567f6073bb9d649d50190b0064
Abhishek Singh <abhisheksdtu@gmail.com>
2024-12-19 14:11:17 +0530

    remove duplicates

--- Leetcode/0083. Remove Duplicates from Sorted List.java
+++ Leetcode/0083. Remove Duplicates from Sorted List.java
@@ -1,17 +1,17 @@
 /*
     APPROACH
-    1. Start with a pointer `curr` at the head of the linked list.
-    2. Traverse the list while `curr.next` is not null.
-    3. If the current node's value is the same as the next node's value, skip the next node by adjusting the pointers (`curr.next = curr.next.next`).
-    4. If the values are different, move `curr` to the next node.
+    1. Start with a pointer curr at the head of the linked list.
+    2. Traverse the list while curr.next is not null.
+    3. If the current node's value is the same as the next node's value, skip the next node by adjusting the pointers (curr.next = curr.next.next).
+    4. If the values are different, move curr to the next node.
     5. Continue this process until the end of the list.
     6. Return the modified list, starting from the head.
 
     TIME COMPLEXITY
-    - O(n), where `n` is the number of nodes in the linked list, as we traverse the entire list once.
+    - O(n), where n is the number of nodes in the linked list, as we traverse the entire list once.
 
     SPACE COMPLEXITY
-    - O(1), since no additional space is used, except for the pointer `curr`.
+    - O(1), since no additional space is used, except for the pointer curr.
 */
 
 class Solution {
