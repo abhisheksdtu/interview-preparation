5607319a1fe70fdc18ee6eb7217802e0ee59cd57
abhisheksdtu <abhisheksdtu@gmail.com>
2025-01-21 18:28:44 +0000

    lc-1903

--- Leetcode/1903. Largest Odd Number in String.java
+++ Leetcode/1903. Largest Odd Number in String.java
@@ -31,3 +31,47 @@ class Solution {
         return "";
     }
 }
+
+// -------------------- IF WE HAVE TO REMOVE STARTING ZEROS -------------------- 
+
+/*
+    APPROACH
+    1. Traverse the string from right to left to find the index of the last odd digit.
+    2. If no odd digit is found, return an empty string.
+    3. Remove leading zeros from the substring up to the last odd digit.
+    4. Return the resulting substring as the largest odd number.
+
+    TIME COMPLEXITY
+    - O(n), where n is the length of the string. Each character is processed at most once.
+
+    SPACE COMPLEXITY
+    - O(1), as no additional data structures are used.
+*/
+
+class Solution {
+    public String largestOddNumber(String num) {
+        int idx = -1;
+
+        // Traverse from right to find the last odd digit
+        for (int i = num.length() - 1; i >= 0; i--) {
+            if ((num.charAt(i) - '0') % 2 != 0) {
+                idx = i;
+                break;
+            }
+        }
+
+        // If no odd digit is found, return empty string
+        if (idx == -1) {
+            return "";
+        }
+
+        // Remove leading zeros up to the index of the last odd digit
+        int i = 0;
+        while (i <= idx && num.charAt(i) == '0') {
+            i++;
+        }
+
+        // Return the substring from the first non-zero to the last odd digit
+        return num.substring(i, idx + 1);
+    }
+}
