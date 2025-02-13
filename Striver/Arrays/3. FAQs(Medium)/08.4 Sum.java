9f2d1d3316049fcf78d2db06f10b624e87f3f4a9
abhisheksdtu <abhisheksdtu@gmail.com>
2025-02-13 09:13:01 +0000

    code

--- Striver/Arrays/3. FAQs(Medium)/08.4 Sum.java
+++ Striver/Arrays/3. FAQs(Medium)/08.4 Sum.java
@@ -21,3 +21,28 @@ class Solution {
         return res;
     }
 }
+
+class Solution {
+  public List<List<Integer>> fourSum(int[] nums, int target) {
+    List<List<Integer>> res = new ArrayList<>();
+    Set<List<Integer>> set = new HashSet<>();
+    int n = nums.length;
+    for (int i = 0; i < n; i++) {
+      List<Integer> list = new ArrayList<>();
+      for (int j = i + 1; j < n; j++) {
+        Set<Integer> s = new HashSet<>();
+        for (int k = j + 1; k < n; k++) {
+          int num = target - (nums[i] + nums[j] + nums[k]);
+          if (s.contains(num)) {
+            Collections.addAll(list, nums[i], nums[j], nums[k], num);
+            Collections.sort(list);
+            set.add(list);
+          }
+          s.add(nums[k]);
+        }
+      }
+    }
+    res.addAll(set);
+    return res;
+  }
+}
