e67134bbdc82b8bb5c3ada07fd1b0c2059d40565
abhisheksdtu <abhisheksdtu@gmail.com>
2025-02-16 17:56:02 +0000

    Search X in sorted array

--- Striver/Binary Search/1.Fundamentals/1.Search X in sorted array.java
+++ Striver/Binary Search/1.Fundamentals/1.Search X in sorted array.java
@@ -30,3 +30,26 @@ class Solution {
     return -1; // Target not found
   }
 }
+
+
+class Solution { 
+    public int search(int[] nums, int target) {
+        return binarySearch(nums,0,nums.length-1,target);
+    }
+
+    public int binarySearch(int[] nums,int lo,int hi, int target){
+        if(lo>hi){
+            return -1;
+        }
+
+        int mid = lo+(hi-lo)/2;
+
+        if(nums[mid]==target){
+            return mid;
+        }else if(nums[mid]>target){
+            return binarySearch(nums,lo,mid-1,target);
+        }else{
+            return binarySearch(nums,mid+1,hi,target);
+        }
+    }
+}
